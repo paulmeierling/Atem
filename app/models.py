0f33d9d4df99819b0858430a48f7cee588a34b13
Paul Meierling <p.meierling@gmail.com>
2019-08-12 13:50:58 -0400

    Models.py changed to include overview

--- app/models.py
+++ app/models.py
@@ -1,13 +1,26 @@
 from app import db 
 
+class Actuation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    datetime = db.Column(db.Date(), index=True)
+    description  = db.Column(db.String(400), default="Run description")
+
+    avg_inflow = db.Column(db.Float(), index=True)
+    start_breath = db.Column(db.Float(), index=True)
+    end_breath = db.Column(db.Float(), index=True)
+    sensor_data = db.relationship('Sensor_data', backref='Actuation', lazy=True)
+
+    def __str__(self):
+        return str(description)
+
 class Sensor_data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    collection_number = db.Column(db.Integer(), index=True)
+    actuation_id = db.Column(db.Integer, db.ForeignKey(Actuation.id))
     time_stamp = db.Column(db.Float(), index=True)
     pressure = db.Column(db.Float(), index=True)
     proximity = db.Column(db.Float(), index=True)
     
     def __str__(self):
-        return str(self.collection_number) + "," + str(self.time_stamp) + "," + str(self.pressure) + "," + str(self.temperature)
+        return str(self.collection_number) + "," + str(self.time_stamp) + "," + str(self.pressure) + "," + str(self.proximity)
 
 
